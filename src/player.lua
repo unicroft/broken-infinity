9a7199bc11c10c3b07a6cbecc4ab161bf1a0cbe5
MrAaaah <jules.minier@mraaaah.fr>
2013-10-11 22:53:37 -0400

    Utilisation de hump pour les classes...

--- src/player.lua
+++ src/player.lua
@@ -1,17 +1,15 @@
-require 'src.class'
+Class = require 'src.hump.class'
 
-Player = class(function(p, name, x, y, size)
-    -- init
-    p.name = name
-    p.x = x
-    p.y = y
-    p.size = size
+Player = Class {
+    init = function(self, name, x, y, size)
+        self.name = name
+        self.x = x
+        self.y = y
+        self.size = size
     end
-)
+}
 
 function Player:draw()
     love.graphics.setColor(255, 0, 0, 128)
     love.graphics.rectangle('fill', self.x, self.y, self.size, self.size)
 end
-
-return Player
